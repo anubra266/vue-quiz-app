de0e2e1a81836a4269062699bfc2ecfb9a335536
anubra266 <anubra266@gmail.com>
2020-11-17 22:51:19 +0100

    Fill Question Box

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <b-jumbotron>
+
+    <template #lead>
+      Question
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      List of Answers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+export default {
+  name: "QuestionBox",
+};
+</script>
