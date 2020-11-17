a4f01eaf5bfb6b37c4379ffa992fd140392fc0dc
anubra266 <anubra266@gmail.com>
2020-11-18 00:00:09 +0100

    User select answer

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,9 +8,14 @@
       <hr class="my-4" />
 
       <b-list-group>
-        <b-list-group-item v-for="(answer, i) in answers" :key="i">{{
-          answer
-        }}</b-list-group-item>
+        <b-list-group-item
+          v-for="(answer, i) in answers"
+          :key="i"
+          @click="selectAnswer(i)"
+          :class="[selectedIndex === i ? 'selected' : '']"
+        >
+          {{ answer }}
+        </b-list-group-item>
       </b-list-group>
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button variant="success" href="#" @click="next">Next</b-button>
@@ -22,11 +27,21 @@
 export default {
   name: "QuestionBox",
   props: { currentQuestion: Object, next: Function },
+  data() {
+    return {
+      selectedIndex: null,
+    };
+  },
   computed: {
     answers() {
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
       return answers;
+    },
+  },
+  methods: {
+    selectAnswer(i) {
+      this.selectedIndex = i;
     },
   },
 };
@@ -35,8 +50,22 @@ export default {
 <style scoped>
 .list-group {
   margin-bottom: 15px;
+  cursor: pointer;
+}
+.list-group-item:hover {
+  background: #eee;
 }
 .btn {
   margin: 0 5px;
+}
+
+.selected {
+  background: lightblue;
+}
+.correct {
+  background: lightgreen;
+}
+.incorrect {
+  background: red;
 }
 </style>
