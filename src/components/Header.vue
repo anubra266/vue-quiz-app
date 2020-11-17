4d74f386699eb17411247d35cd740f097b61db65
anubra266 <anubra266@gmail.com>
2020-11-17 22:50:55 +0100

    Fill Header

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <b-nav tabs>
+    <b-nav-item disabled><b>Vue Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter: 4/10</b-nav-item>
+  </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Header"
+    }
+</script>
