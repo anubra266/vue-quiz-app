ce25966124a73ddaab0a1040e03e94832d68d0a3
anubra266 <anubra266@gmail.com>
2020-11-18 00:18:26 +0100

    implement Counter in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,15 @@
 <template>
-    <div>
-        <b-nav tabs>
-    <b-nav-item disabled><b>Vue Quiz App</b></b-nav-item>
-    <b-nav-item disabled>Counter: 4/10</b-nav-item>
-  </b-nav>
-    </div>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><b>Vue Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
-    export default {
-        name:"Header"
-    }
+export default {
+  name: "Header",
+  props: ["numCorrect", "numTotal"],
+};
 </script>
